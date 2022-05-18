f6f9ed176ee8ab561fdec84b72f432f7313046a7
Kishan Shah <kishanshah@kishans-air.myfiosgateway.com>
2022-05-18 18:14:56 -0400

    Initial commit

---
+++ Develop/script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+// Create function generatePassword
+function generatePassword() {
+  // ...
+
+
+
+  return aPassString; // "fsajdfhsakdjfs"
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
