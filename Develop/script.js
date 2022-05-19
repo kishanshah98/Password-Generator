17f23b0b92397516aa09b37c34c37a8d9ff53ff8
Kishan Shah <kishanshah@kishans-air.myfiosgateway.com>
2022-05-19 17:55:35 -0400

    For-loop and If Statements Made

--- Develop/script.js
+++ Develop/script.js
@@ -1,58 +1,84 @@
 // Assignment Code 
-var specialCharacters = ['!', "\'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '`', '{', '|', '}', '~'];
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-console.log(specialCharacters);
-console.log(numbers);
-console.log(upperCaseLetters);
-console.log(lowerCaseLetters);
+var specialCharactersList = ['!', "\'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '`', '{', '|', '}', '~'];
+var numbersList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var upperCaseLettersList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseLettersList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+console.log(specialCharactersList);
+console.log(numbersList);
+console.log(upperCaseLettersList);
+console.log(lowerCaseLettersList);
 
 function options() {
+  // Creation of Password length integer
   var passwordLength = parseInt(window.prompt("How many characters would you like your password to be?"));
   console.log(passwordLength);
-  
+  // Storing whether or not the user wants the following in their password
   var ifspecialCharacters = window.confirm("Would you like to include special characters?");
   var ifnumbers = window.confirm("Would you like to include numeric characters?");
   var ifupperCaseLetters = window.confirm("Would you like to include uppercase characters?");
   var iflowerCaseLetters = window.confirm("Would you like to include lowercase characters?");
-  
+
   console.log(ifspecialCharacters);
   console.log(ifnumbers);
   console.log(ifupperCaseLetters);
   console.log(iflowerCaseLetters);
 
+  // Creation of object with stored elements
   var userInput = {
-    length: passwordLength,
+    passLength: passwordLength,
+    specialChar: ifspecialCharacters,
     numbers: ifnumbers,
     upperCase: ifupperCaseLetters,
     lowerCase: iflowerCaseLetters,
-    specialChar: ifspecialCharacters,
-  }
-  console.log(userInput)
+  };
+
+  console.log(userInput);
+  // Returns object to the caller
   return userInput;
 }
-options();
+
 var generateBtn = document.querySelector("#generate");
 // Create function generatePassword
 function generatePassword() {
-  var choices = options()
-  var chars_concat = [];
-  var chars_password = [];
+  var choices = options();
 
-  console.log(chars_concat)
-
-
-  for (var i = 0; i < passwordLength; i++) {
-    var randomNum = Math.floor(Math.random() * chars_concat.length);
-    chars_password[randomNum]
-
+  if (!choices) {
+      return;
   }
 
+  var charsConcat = [];
+  var charsPassword = [];
+
+  console.log(charsConcat);
+  console.log(charsPassword);
+
+  if (choices.specialChar) {
+    charsConcat = charsConcat.concat(specialCharactersList);
+  }
+  if (choices.numbers) {
+    charsConcat = charsConcat.concat(numbersList);
+  }
+  if (choices.upperCase) {
+    charsConcat = charsConcat.concat(upperCaseLettersList);
+  }
+  if (choices.lowerCase) {
+    charsConcat = charsConcat.concat(lowerCaseLettersList);
+  }
+  console.log(charsConcat);
+  console.log(charsConcat.length);
+  console.log(choices.passLength);
+
+  for (var i = 0; i < choices.passLength; i++) {
+    var randomNum = Math.floor(Math.random() * charsConcat.length);
+    charsPassword = charsConcat[randomNum];
+  }
+ //*** troubleshoot this ***  console.log(charsPassword);
+  // return aPassString; // "fsajdfhsakdjfs"
+};
+
+generatePassword();
 
 
-  return aPassString; // "fsajdfhsakdjfs"
-}
 
 // Write password to the #password input
 function writePassword() {
